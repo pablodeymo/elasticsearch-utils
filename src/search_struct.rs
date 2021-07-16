b7ee8413f0e078a531b3be2a2c5311fd17a7e763
Pablo Deymonnaz <pdeymon@fi.uba.ar>
2021-07-16 02:19:47 -0300

    Added fields

--- src/search_struct.rs
+++ src/search_struct.rs
@@ -12,13 +12,19 @@ use std::collections::HashMap;
 ///         }
 ///     }
 /// }
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SearchStruct {
     pub query: BoolQuery,
+    pub fields: Vec<String>,
+    #[serde(rename = "_source")]
+    pub source: bool,
 }
 
 impl SearchStruct {
-    pub fn new_from_hashmap_conditions(cond: HashMap<String, String>) -> SearchStruct {
+    pub fn new_from_hashmap_conditions(
+        cond: HashMap<String, String>,
+        fields_str: Option<&str>,
+    ) -> SearchStruct {
         let mut vec: Vec<MatchQuery> = Vec::new();
         for (k, v) in cond {
             let mut cond_internal: HashMap<String, String> = HashMap::new();
@@ -28,25 +34,34 @@ impl SearchStruct {
                 match_cond: cond_internal,
             });
         }
+        // convertir lista de campos separada por comas en un vector de strings
+        let mut fields: Vec<String> = Vec::new();
+        if let Some(fields_value) = fields_str {
+            for field in fields_value.split(',') {
+                fields.push(String::from(field));
+            }
+        }
         SearchStruct {
             query: BoolQuery {
                 bool: MustQuery { must: vec },
             },
+            fields,
+            source: false,
         }
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct BoolQuery {
     pub bool: MustQuery,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MustQuery {
     pub must: Vec<MatchQuery>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MatchQuery {
     #[serde(rename(serialize = "match"))]
     pub match_cond: HashMap<String, String>,
@@ -73,6 +88,8 @@ mod tests {
                     ],
                 },
             },
+            fields: Vec::new(),
+            source: false,
         };
 
         println!("{:?}", serde_json::to_string(&q).unwrap());
